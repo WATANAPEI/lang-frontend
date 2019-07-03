28601f9c162b40e486000a578de8bfbda013c0f1
WATANAPEI <info@wpei.dev>
2019-07-03 17:35:20 +0200

    *Main.tsx  -> add fetch logic  TODO: check cors config on puma

--- client/components/Main.tsx
+++ client/components/Main.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 // @ts-ignore
 import WordCard from "./WordCard.tsx";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import Button from "@material-ui/core/Button"
+import Button from "@material-ui/core/Button";
+// @ts-ignore
+import useWordApi from "../hooks/useWordApi.tsx";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -28,15 +30,35 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function Main() {
   const classes = useStyles();
+  const [id, setId] = useState(1);
+  const [{ wordResponse, isLoading, isError }, doFetch] = useWordApi(
+    "http://localhost:3000/words/1",
+    {
+      word: "aa",
+      meaning: "bb",
+      wordLanguage: "English",
+      meaningLanguage: "Tumbuka"
+    }
+  );
   return (
     <Grid container spacing={2} className={classes.mainContainer}>
       <Grid item xs={1} className={classes.navCard}>
-        <Button>
+        <Button
+          onClick={() => {
+            setId(id + 1);
+            doFetch("http://localhost:3000/words/${id}");
+          }
+       } >
           <ArrowBackIcon />
         </Button>
       </Grid>
       <Grid item xs={10} className={classes.wordCardGrid}>
-        <WordCard text="this is a word card." />
+        {isError && <div>Something went wrong ...</div>}
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <WordCard text={wordResponse.word} />
+        )}
       </Grid>
       <Grid item xs={1} className={classes.navCard}>
         <Button>
