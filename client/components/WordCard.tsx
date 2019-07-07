2c47c80cf05d9717dfff7b9dbebc9365da766a10
WATANAPEI <info@wpei.dev>
2019-07-07 10:25:02 +0200

    *WordCard *style.css -> add click event and animation with useState

--- client/components/WordCard.tsx
+++ client/components/WordCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/style.css";
 // @ts-ignore
 
@@ -11,9 +11,21 @@ interface WordResponse {
 }
 
 function WordCard(word: WordResponse) {
+  const [isFlipped, setIsFlipped] = useState(false);
   return (
     <div className={styles.flipCard}>
-      <div className={styles.flipCardInner}>
+      <div
+        className={styles.flipCardInner}
+        onClick={(event: React.MouseEvent<HTMLInputElement>): void => {
+          if (!isFlipped) {
+            event.currentTarget.style.transform = "rotateY(180deg)";
+            setIsFlipped(true);
+          } else {
+            event.currentTarget.style.transform = "rotateY(0deg)";
+            setIsFlipped(false);
+          }
+        }}
+      >
         <div className={styles.flipCardFront}>{word.word}</div>
         <div className={styles.flipCardBack}>{word.meaning}</div>
       </div>
