957cf317804e80ce13ad6d58074f9df9ce36a692
WATANAPEI <info@wpei.dev>
2019-07-08 17:24:33 +0200

    *HeaderBar.spec.tsx -> add material-ui test

--- client/__test__/HeaderBar.spec.tsx
+++ client/__test__/HeaderBar.spec.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { createShallow, createMount } from "@material-ui/core/test-utils";
 // @ts-ignore
 import HeaderBar from "../components/HeaderBar.tsx";
+import AppBar from "@material-ui/core/AppBar";
 
 describe("HeaderBar", () => {
   it("display the received text", () => {
     const testStr = "this is test";
-    const headerBar = shallow(<HeaderBar text={testStr} />);
-
-    expect(headerBar.text()).toEqual(testStr);
+    const wrapper = createShallow()(<HeaderBar text={testStr} />);
+    expect(wrapper.text()).toEqual(testStr);
 //    expect(shallow).toMatchSnapshot();
+  });
+
+  it("has an AppBar", () => {
+    const testStr = "this is test";
+    const wrapper = createMount()(<HeaderBar text={testStr} />);
+    expect(wrapper.find(AppBar)).toHaveLength(1);
   });
 });
