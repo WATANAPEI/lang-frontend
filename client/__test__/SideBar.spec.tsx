5a950521b9926ecb7ec29fb6999b163fe0fac80a
WATANAPEI <info@wpei.dev>
2019-07-08 22:54:42 +0200

    +SideBar.spec -> add SideBar test

---
+++ client/__test__/SideBar.spec.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createShallow, createMount } from "@material-ui/core/test-utils";
+// @ts-ignore
+import SideBar from "../components/SideBar.tsx";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
+
+describe("</SideBar />", () => {
+  it("has a MenuList and MenuItems", () => {
+    const testText = "sideBar text";
+    const wrapper = createShallow()(<SideBar text={testText} />);
+    expect(wrapper.find(MenuList)).toHaveLength(1);
+    expect(wrapper.find(MenuItem)).toBeTruthy();
+  });
+  it("render received texts", () => {
+    const testText = "sideBar text";
+    const wrapper = createShallow()(<SideBar text={testText} />);
+    console.log(wrapper.text());
+    expect(wrapper.text()).toEqual(
+      expect.stringContaining(testText));
+  });
+});
