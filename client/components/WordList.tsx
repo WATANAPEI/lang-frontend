ecfab7e6d88165c7d69ab178ccb50feef1d30e3e
WATANAPEI <info@wpei.dev>
2019-07-19 17:22:17 +0200

    add frame for wordList

--- client/components/WordList.tsx
+++ client/components/WordList.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function WordList() {
-  return <div>this is just dummy</div>;
+  const fetchWordList = [
+    {
+      id: 1,
+      word: "word1",
+      meaning: "meaning1"
+    },
+    {
+      id: 2,
+      word: "word2",
+      meaning: "meaning2"
+    },
+    {
+      id: 3,
+      word: "word3",
+      meaning: "meaning3"
+    }
+  ];
+
+  const wordListItems = fetchWordList.map(word =>
+    <li key={word.id.toString()}>
+      {word.word} | {word.meaning}
+    </li>
+  );
+
+  return <ul>{wordListItems}</ul>;
 }
 
 export default WordList;
