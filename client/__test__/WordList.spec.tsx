6a332dae35ab29729da1060b7a7694ac276b6fd0
WATANAPEI <info@wpei.dev>
2019-07-22 21:39:29 +0200

    +WordList.spec -> add frameword

---
+++ client/__test__/WordList.spec.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+// @ts-ignore
+import WordList from "../components/WordList.tsx";
+import { createMount } from "@material-ui/core/test-utils";
+
+describe("<WordList />", () => {
+  it.todo("has Paper");
+  it.todo("has progress indication");
+  it.todo("has word.id and word.word and word.meaning");
+});
