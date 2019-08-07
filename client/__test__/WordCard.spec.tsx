3487bed63ea6c81e33c5df6fec2b389770a68616
WATANAPEI <info@wpei.dev>
2019-08-07 10:04:54 +0200

    *MainContainer.spec -> just renamed *WordCard.spec -> fix test according to WordCard fix

--- client/__test__/WordCard.spec.tsx
+++ client/__test__/WordCard.spec.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import { mount, shallow } from "enzyme";
 // @ts-ignore
-import WordCard from "../components/WordCard.tsx";
+import WordCard, { WordCardProps } from "../components/WordCard.tsx";
 // @ts-ignore
-import { WordResponse } from "../components/WordCard.tsx";
 import toJson from "enzyme-to-json";
 
 describe("<WordCard />", () => {
-  const testWord: WordResponse = {
-    id: 1,
-    word: "test word",
-    meaning: "test meaning",
-    wordLanguageID: 1,
-    meaningLanguageID: 2
+  const testWord = {
+    wordFront: "word_written_in_front",
+    wordBack: "word_written_in_back"
   };
   const wrapper = mount(<WordCard {...testWord} />);
   afterAll(() => {
     wrapper.unmount();
-  })
+  });
 
   it("has 4 divs with flipCard/flipCardInner/flipCardFront/flipCardBack class", () => {
     expect(wrapper.find("div.flipCard")).toHaveLength(1);
@@ -29,11 +25,11 @@ describe("<WordCard />", () => {
   });
 
   it("has a card with word", () => {
-    expect(wrapper.find(".flipCardFront").text()).toEqual(testWord.word);
+    expect(wrapper.find(".flipCardFront").text()).toEqual(testWord.wordFront);
   });
 
   it("has a card with meaning", () => {
-    expect(wrapper.find(".flipCardBack").text()).toEqual(testWord.meaning);
+    expect(wrapper.find(".flipCardBack").text()).toEqual(testWord.wordBack);
   });
 
   it("check the mount rendering doesn't change before and after click", () => {
